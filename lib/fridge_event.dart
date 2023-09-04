dcf2330eb5c6128ef1309b7b8a18488b210e1708
Tommaso Coianiz <coianiz.tommaso@spes.uniud.it>
2023-09-05 01:46:01 +0200

    Implementazione Socket.IO
    Aggiunta la classe FridgeEvent che si occupa di instaurare una connessione con il server node.js che sfrutta la libreria Socket.IO. Preparati i metodi che servono a inviare e ricevere notifiche di aggiornamenti al database a/da altri utenti che gestiscono la stessa dispensa. Ciò consentirà, in futuro, di aggiornare automaticamente i dati locali agli utenti interessati dalle modifiche.

---
+++ lib/fridge_event.dart
@@ -0,0 +1,76 @@
+/* classe che definisce gli oggetti fridgeEvent. Instaura una connessione con il
+server node e, tramite il package socket.io-client, si mette in ascolto di eventi
+che riguardano l'aggiornamento del database contenente i prodotti nella dispensa
+condivisi con altri utenti. */
+
+import 'package:socket_io_client/socket_io_client.dart' as IO;
+import 'dart:async';
+
+class FridgeEvent {
+  String _fridgeID = ""; // ID del frigo, da definire altrove
+  String _ipaddress = "";
+  String _port = "";
+  String _url = "";
+  late IO.Socket socket;
+
+  static final FridgeEvent _instance = FridgeEvent._internal();
+
+  factory FridgeEvent() {
+    return _instance;
+  }
+
+  FridgeEvent._internal() {
+    this._ipaddress = "localhost";
+    this._port = "3000";
+    this._fridgeID = "prova";
+    this._url = "http://" + this._ipaddress + ":" + this._port;
+    this.socket = IO.io(this._url, <String, dynamic>{
+      'transports': ['websocket'],
+      'autoConnect': false,
+    });
+  }
+
+  /* funzione che si occupa di instaurare la connessione con il server node e di
+  mettersi in ascolto di eventi che riguardano l'aggiornamento del database
+   */
+  Future<void> communicate() async {
+    this.socket = IO.io(this._url, <String, dynamic>{
+      'transports': ['websocket'],
+      'autoConnect': false,
+    });
+
+    await socket.connect();
+
+    socket.onConnect((_) {
+      print("Connected");
+      // invia l'id del frigo al server node
+      socket.emit("fridgeID", this._fridgeID);
+    });
+
+    socket.onConnectError((error) {
+      print("Connection Error: $error");
+    });
+
+    socket.onDisconnect((_) {
+      print("Disconnected");
+    });
+
+    socket.on("fridgeEvent", (data) {
+      print(data);
+      /* qui va inserita la funzione che aggiorna i dati locali prendendoli dal
+      database remoto */
+    });
+
+  }
+
+  // invia un messaggio di update al server node
+  void sendUpdate() {
+    print("sending update");
+    this.socket.emit("fridgeUpdate", this._fridgeID);
+  }
+
+  // interrompe la comunicazione con il server node
+  void stopCommunication() {
+    this.socket.disconnect();
+  }
+}
